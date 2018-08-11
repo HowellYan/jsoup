ef6edc4a201ba83754e33b1ad0b8424ae2e7f092
HowellYang <1726474652@qq.com>
2018-08-11 19:08:42 +0800

    v6

---
+++ src/main/java/cn/com/howell/jiuxian/ReptileGoodsInfo.java
@@ -0,0 +1,264 @@
+package cn.com.howell.jiuxian;
+
+
+public class ReptileGoodsInfo {
+
+  private long id;
+  private String goodsSn;
+  private String goodsName;
+  private String subTitle;
+  private String goodsType;
+  private String goodsBrand;
+  private String placeOrigin;
+  private double price;
+  private String cover;
+  private String ingestingTime;
+  private String bottleStopper;
+  private String scent;
+  private String grapeVariety;
+  private String collocationFood;
+  private String storageConditions;
+  private String taste;
+  private String netContent;
+  private String color;
+  private String cartonSize;
+  private String introduce;
+  private String winery;
+  private String description;
+  private String materials;
+  private String process;
+  private long collection;
+  private String lbarr;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getGoodsSn() {
+    return goodsSn;
+  }
+
+  public void setGoodsSn(String goodsSn) {
+    this.goodsSn = goodsSn;
+  }
+
+
+  public String getGoodsName() {
+    return goodsName;
+  }
+
+  public void setGoodsName(String goodsName) {
+    this.goodsName = goodsName;
+  }
+
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+
+  public String getGoodsType() {
+    return goodsType;
+  }
+
+  public void setGoodsType(String goodsType) {
+    this.goodsType = goodsType;
+  }
+
+
+  public String getGoodsBrand() {
+    return goodsBrand;
+  }
+
+  public void setGoodsBrand(String goodsBrand) {
+    this.goodsBrand = goodsBrand;
+  }
+
+
+  public String getPlaceOrigin() {
+    return placeOrigin;
+  }
+
+  public void setPlaceOrigin(String placeOrigin) {
+    this.placeOrigin = placeOrigin;
+  }
+
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+
+  public String getCover() {
+    return cover;
+  }
+
+  public void setCover(String cover) {
+    this.cover = cover;
+  }
+
+
+  public String getIngestingTime() {
+    return ingestingTime;
+  }
+
+  public void setIngestingTime(String ingesting_Time) {
+    this.ingestingTime = ingestingTime;
+  }
+
+
+  public String getBottleStopper() {
+    return bottleStopper;
+  }
+
+  public void setBottleStopper(String bottleStopper) {
+    this.bottleStopper = bottleStopper;
+  }
+
+
+  public String getScent() {
+    return scent;
+  }
+
+  public void setScent(String scent) {
+    this.scent = scent;
+  }
+
+
+  public String getGrapeVariety() {
+    return grapeVariety;
+  }
+
+  public void setGrapeVariety(String grapeVariety) {
+    this.grapeVariety = grapeVariety;
+  }
+
+
+  public String getCollocationFood() {
+    return collocationFood;
+  }
+
+  public void setCollocationFood(String collocationFood) {
+    this.collocationFood = collocationFood;
+  }
+
+
+  public String getStorageConditions() {
+    return storageConditions;
+  }
+
+  public void setStorageConditions(String storageConditions) {
+    this.storageConditions = storageConditions;
+  }
+
+
+  public String getTaste() {
+    return taste;
+  }
+
+  public void setTaste(String taste) {
+    this.taste = taste;
+  }
+
+
+  public String getNetContent() {
+    return netContent;
+  }
+
+  public void setNetContent(String netContent) {
+    this.netContent = netContent;
+  }
+
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+
+  public String getCartonSize() {
+    return cartonSize;
+  }
+
+  public void setCartonSize(String cartonSize) {
+    this.cartonSize = cartonSize;
+  }
+
+
+  public String getIntroduce() {
+    return introduce;
+  }
+
+  public void setIntroduce(String introduce) {
+    this.introduce = introduce;
+  }
+
+
+  public String getWinery() {
+    return winery;
+  }
+
+  public void setWinery(String winery) {
+    this.winery = winery;
+  }
+
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+
+  public String getMaterials() {
+    return materials;
+  }
+
+  public void setMaterials(String materials) {
+    this.materials = materials;
+  }
+
+
+  public String getProcess() {
+    return process;
+  }
+
+  public void setProcess(String process) {
+    this.process = process;
+  }
+
+  public long getCollection() {
+    return collection;
+  }
+
+  public void setCollection(long collection) {
+    this.collection = collection;
+  }
+
+
+  public String getLbarr() {
+    return lbarr;
+  }
+
+  public void setLbarr(String lbarr) {
+    this.lbarr = lbarr;
+  }
+}
